627fe2b80103163cc1f155fa60e7a37d3114f4c8
oboy21 <oboy11b@gmail.com>
2024-03-31 12:47:20 +0300

    Day 22: Render the app titles in two columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -13,6 +14,17 @@ with col2:
 
     st.info(content)
 
-content2 = """Below you canfind some of the apps I have built in Python. Feel free to ask anything"""
+    content2 = """Below you canfind some of the apps I have built in Python. Feel free to ask anything"""
 
-st.write(content2)
+    st.write(content2)
+
+col3, col4  = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df.iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
